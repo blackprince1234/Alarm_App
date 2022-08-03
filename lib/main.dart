59d0ff51cef57e6e22e051848fd8ef1d98f7b4f1
ryanoh0303 <ryano35961@gmail.com>
2022-08-03 03:01:41 -0700

    more app

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_app/addAlarm.dart';
+//import 'package:numberpicker/numberpicker.dart';
 
 void main() => runApp(
       MaterialApp(
@@ -21,70 +22,169 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
           appBar: AppBar(
             backgroundColor: const Color(0xff02343F),
-            leading: IconButton(
-              icon: Icon(
-                Icons.menu,
-                size: 50, //size of the icon
-                color: Colors.white,
-              ),
-              onPressed: () {
-                showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        scrollable: true,
-                        title: Text('Add New Alarm'),
-                        content: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Form(
-                            child: Column(
-                              children: <Widget>[
-                                TextFormField(
-                                  decoration: InputDecoration(
-                                    labelText: 'Name',
-                                    icon: Icon(Icons.account_box),
-                                  ),
-                                ),
-                                TextFormField(
-                                  decoration: InputDecoration(
-                                    labelText: 'Email',
-                                    icon: Icon(Icons.email),
-                                  ),
-                                ),
-                                TextFormField(
-                                  decoration: InputDecoration(
-                                    labelText: 'Message',
-                                    icon: Icon(Icons.message),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        actions: [
-                          RaisedButton(
-                              child: Text("Submit"),
-                              onPressed: () {
-                                // your code
-                              })
-                        ],
-                      );
-                    });
-              },
-            ),
             actions: [Icon(Icons.access_alarms)],
             title: Text(
               'Alarm',
               style: TextStyle(color: const Color(0xffF0EDCC)),
             ),
           ),
-          body: Container(child: Icon(Icons.star)),
+          body:Container(
+            padding:EdgeInsets.all(10),
+            child:Stack(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: FloatingActionButton(
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              scrollable: true,
+                              title: Text('Add New Alarm'),
+                              content: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Form(
+                                  child: Column(
+                                    children: <Widget>[
+                                      TextFormField(
+                                        decoration: InputDecoration(
+                                          labelText: 'Name',
+                                          icon: Icon(Icons.account_box),
+                                        ),
+                                      ),
+                                      TextFormField(
+                                        decoration: InputDecoration(
+                                          labelText: 'Email',
+                                          icon: Icon(Icons.email),
+                                        ),
+                                      ),
+                                      TextFormField(
+                                        decoration: InputDecoration(
+                                          labelText: 'Message',
+                                          icon: Icon(Icons.message),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              actions: [
+                                RaisedButton(
+                                    child: Text("Submit"),
+                                    onPressed: () {
+                                      // your code
+                                    })
+                              ],
+                            );
+                          });
+                    },
+                    backgroundColor: Colors.green,
+                    child: const Icon(Icons.add_alarm_rounded),      //icon for the screen.
+                  ),
+                  ),
+
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: FloatingActionButton(
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              scrollable: true,
+                              title: Text('Add New Alarm'),
+                              content: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Form(
+                                  child: Column(
+                                    children: <Widget>[
+                                      TextFormField(
+                                        decoration: InputDecoration(
+                                          labelText: 'Name',
+                                          icon: Icon(Icons.account_box),
+                                        ),
+                                      ),
+                                      TextFormField(
+                                        decoration: InputDecoration(
+                                          labelText: 'Email',
+                                          icon: Icon(Icons.email),
+                                        ),
+                                      ),
+                                      TextFormField(
+                                        decoration: InputDecoration(
+                                          labelText: 'Message',
+                                          icon: Icon(Icons.message),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              actions: [
+                                RaisedButton(
+                                    child: Text("Submit"),
+                                    onPressed: () {
+                                      // your code
+                                    })
+                              ],
+                            );
+                          });
+                    },
+                    backgroundColor: Colors.green,
+                    child: const Icon(Icons.remove),      //icon for the screen.
+                  ),
+                  ),
+              ],
+            ),
+
+          ),
+
+          drawer: Drawer(
+            // Add a ListView to the drawer. This ensures the user can scroll
+            // through the options in the drawer if there isn't enough vertical
+            // space to fit everything.
+            child: ListView(
+              // Important: Remove any padding from the ListView.
+              padding: EdgeInsets.zero,
+              children: [
+                const DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: const Color(0xff02343F),
+                  ),
+                  child: Text(
+                      'Options',
+                      style: TextStyle(height: 5, fontSize: 30, color:const Color(0xffF0EDCC)),
+                  ),
+                ),
+                ListTile(
+                  title: const Text('Item 1'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: const Text('Item 2'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          ),
+
+
           bottomNavigationBar: BottomAppBar(
+
             child: SizedBox(
               height: 70,
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
+
                   IconButton(
                       icon: Icon(
                         Icons.exit_to_app,
