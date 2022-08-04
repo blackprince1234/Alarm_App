ed10c299ea0ca7a1a6ae5d43c9e5259bb84b14b6
ryanoh0303 <ryano35961@gmail.com>
2022-08-03 20:36:51 -0700

    Added another number picker (for the minutes)
    Now, I need to make sure the number goes back to the minimum after maximum, and vice versa.
    
    For instance, after 59, the minute goes back to 0.

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,15 @@ void main() => runApp(
         home: MyDemo(),
       ),
     );
+
 class MyDemo extends StatefulWidget {
   @override
   MyApp createState() => MyApp();
 }
+
 //asdljkfasd
 class MyApp extends State<MyDemo> {
- // const MyApp({Key? key}) : super(key: key);
+  // const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -33,66 +35,69 @@ class MyApp extends State<MyDemo> {
               style: TextStyle(color: const Color(0xffF0EDCC)),
             ),
           ),
-          body:Container(
-            padding:EdgeInsets.all(10),
-            child:Stack(
+          body: Container(
+            padding: EdgeInsets.all(10),
+            child: Stack(
               children: <Widget>[
                 Align(
                   alignment: Alignment.bottomLeft,
                   child: FloatingActionButton(
                     onPressed: () {
-                      showDialog<int>(
+                      showDialog(
                           context: context,
                           builder: (BuildContext context) {
                             return AlertDialog(
                               scrollable: true,
                               title: Text('Add New Alarm'),
+                              content:
+                                  Row(
+                                    children: [
+                                      StatefulBuilder(builder: (context, setState) {
+                                        return NumberPicker(
+                                          selectedTextStyle:
+                                          TextStyle(color: Colors.red),
+                                          value: currentValue,
+                                          minValue: 0,
+                                          maxValue: 23,
+                                          onChanged: (value) =>
+                                              setState(() => currentValue = value),
+                                        );
+                                      }),
+                                      StatefulBuilder(builder: (context, setState) {
+                                        return NumberPicker(
+                                          selectedTextStyle:
+                                          TextStyle(color: Colors.red),
+                                          value: currentValue,
+                                          minValue: 0,
+                                          maxValue: 59,
+                                          onChanged: (value) =>
+                                              setState(() => currentValue = value),
+                                        );
+                                      }),
+                                      Text(
+                                        'PM',
+                                      )
+                                    ]
 
-
-                              content: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Form(
-                                  child: Column(
-                                    children: <Widget>[
-                                      //fix
-                                      NumberPicker(
-                                        selectedTextStyle: TextStyle(color: Colors.red),
-                                        value: currentValue,
-                                        minValue: 1,
-                                        maxValue: 4,
-                                        onChanged: (value) => setState(() => currentValue = value),
-                                      ),
-                                      TextFormField(
-                                        decoration: InputDecoration(
-                                          labelText: 'Email',
-                                          icon: Icon(Icons.email),
-                                        ),
-                                      ),
-                                      TextFormField(
-                                        decoration: InputDecoration(
-                                          labelText: 'Message',
-                                          icon: Icon(Icons.message),
-                                        ),
-                                      ),
-                                    ],
                                   ),
-                                ),
-                              ),
+
+
+
                               actions: [
                                 RaisedButton(
                                     child: Text("Submit"),
                                     onPressed: () {
-                                      // your code
+                                      //when the submit button is pressed
                                     })
                               ],
                             );
                           });
                     },
                     backgroundColor: Colors.green,
-                    child: const Icon(Icons.add_alarm_rounded),      //icon for the screen.
+                    child: const Icon(
+                        Icons.add_alarm_rounded), //icon for the screen.
                   ),
-                  ),
-
+                ),
                 Align(
                   alignment: Alignment.bottomRight,
                   child: FloatingActionButton(
@@ -141,14 +146,12 @@ class MyApp extends State<MyDemo> {
                           });
                     },
                     backgroundColor: Colors.green,
-                    child: const Icon(Icons.remove),      //icon for the screen.
+                    child: const Icon(Icons.remove), //icon for the screen.
                   ),
-                  ),
+                ),
               ],
             ),
-
           ),
-
           drawer: Drawer(
             // Add a ListView to the drawer. This ensures the user can scroll
             // through the options in the drawer if there isn't enough vertical
@@ -162,8 +165,11 @@ class MyApp extends State<MyDemo> {
                     color: const Color(0xff02343F),
                   ),
                   child: Text(
-                      'Options',
-                      style: TextStyle(height: 5, fontSize: 30, color:const Color(0xffF0EDCC)),
+                    'Options',
+                    style: TextStyle(
+                        height: 5,
+                        fontSize: 30,
+                        color: const Color(0xffF0EDCC)),
                   ),
                 ),
                 ListTile(
@@ -184,16 +190,12 @@ class MyApp extends State<MyDemo> {
               ],
             ),
           ),
-
-
           bottomNavigationBar: BottomAppBar(
-
             child: SizedBox(
               height: 70,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-
                   IconButton(
                       icon: Icon(
                         Icons.exit_to_app,
